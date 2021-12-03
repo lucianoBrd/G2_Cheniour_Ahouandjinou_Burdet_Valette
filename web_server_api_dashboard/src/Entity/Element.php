e1e25ecaee7e0061d262b06333d074228848c767
lucianoBrd <lucien.burdet@gmail.com>
2021-12-03 11:25:54 +0100

    fix bug

--- web_server_api_dashboard/src/Entity/Element.php
+++ web_server_api_dashboard/src/Entity/Element.php
@@ -2,11 +2,12 @@
 
 namespace App\Entity;
 
-use ApiPlatform\Core\Annotation\ApiResource;
-use App\Repository\ElementRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use App\Controller\ElementByLabel;
+use App\Repository\ElementRepository;
+use Doctrine\Common\Collections\Collection;
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\Common\Collections\ArrayCollection;
 
 /**
  * @ApiResource(
--- web_server_api_dashboard/src/Entity/Home.php
+++ web_server_api_dashboard/src/Entity/Home.php
@@ -2,11 +2,12 @@
 
 namespace App\Entity;
 
-use ApiPlatform\Core\Annotation\ApiResource;
-use App\Repository\HomeRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
+use App\Controller\HomeByLabel;
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\HomeRepository;
+use Doctrine\Common\Collections\Collection;
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\Common\Collections\ArrayCollection;
 
 /**
  * @ApiResource(
