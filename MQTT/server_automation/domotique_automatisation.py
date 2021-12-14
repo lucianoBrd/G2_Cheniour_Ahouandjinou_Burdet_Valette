8bd026fc5eaa490de3065e2f96a8b79b5a25b320
Valentin Valette <valentin.valette@orange.com>
2021-12-14 16:17:14 +0100

    ajout du traitement des actions non traitées dans le serveur + fonction pour récupérer les objets element et room plus facilement

--- MQTT/server_automation/domotique_automatisation.py
+++ MQTT/server_automation/domotique_automatisation.py
@@ -3,7 +3,9 @@ import time
 from Home import Home
 from Room import Room
 from Element import Element
+from api import API
 from decouple import config
+
 
 BROKER_ADDRESS = '192.168.43.222'
 TOPIC_DICT = {}
@@ -44,7 +46,6 @@ def init_my_home(mqtt_client):
         #sensor
         #actuator
     
-
     rooms_list = [Room(config("ROOM_NAME_1"), living_room_elements_list), Room(config("ROOM_NAME_2"), [])]
     home = Home(config("HOME_NAME"), rooms_list, mqtt_client)
 
@@ -61,18 +62,55 @@ def init_my_home(mqtt_client):
             home.mqtt_client.subscribe(element.topic)
     return home
 
+def check_and_format_actions_api (api_obj):
+    api_obj = API()
+    unresolved_actions = api_obj.get_unresolved_actions()
+
+    unresolved_formated_actions = []
+    if unresolved_actions == []:
+        return None
+    else :
+        for action in unresolved_actions:
+            action_id = action["id"]
+            element_id = action["element"].split("/")[-1]
+            element_name = api_obj.get_element_name_by_id(element_id)
+
+            room_id = api_obj.get_element_by_name(element_name)["room"].split("/")[-1]
+            room_name = api_obj.get_room_name_by_id(room_id)
+
+            home_id = api_obj.get_room_by_name(room_name)["home"].split("/")[-1]
+            home_name = api_obj.get_home_name_by_id(home_id)
+
+            value = action["value"]
+
+            action_formated = {"action_id" : action_id, "home" : home_name, "room" : room_name, "element" : element_name, "value" : value}
+
+            unresolved_formated_actions.append(action_formated)
+        return unresolved_formated_actions
+    
+def get_room_obj(home_obj, room_name):
+    return home_obj.rooms[room_name]
+
+def get_element_obj(home_obj, room_name, element_name):
+    return home_obj.rooms[room_name].elements[element_name]
+
 
 if __name__ == "__main__":
-    mqtt_client = init_mqtt_connection()
-    home = init_my_home(mqtt_client)
+    #mqtt_client = init_mqtt_connection()
+    #home = init_my_home(mqtt_client)
+    home = init_my_home(mqtt.Client("test"))
+    web_api = API()
+
     while True:
         time.sleep(0.5)
 
+        #uniquement en mode automatique ??
         #region check temperature in living room
-        temperature_living_room = float(home.rooms['living_room'].elements['sensor_temperature_living_room'].data['temperature'])
+        #########CREER UN FONCTION POUR RECUPERER OBJETS EN FONCTION DU NOM ETC
+        temperature_living_room = float(get_element_obj(home, 'living_room', 'sensor_temperature_living_room').data['temperature'])
         
-        vmc_living_room = home.rooms['living_room'].elements['actuator_vmc_living_room']
-        heating_living_room = home.rooms['living_room'].elements['actuator_heating_living_room']
+        vmc_living_room = get_element_obj(home, 'living_room', 'actuator_vmc_living_room')
+        heating_living_room = get_element_obj(home, 'living_room', 'actuator_heating_living_room')
         
         if temperature_living_room < 25 and temperature_living_room > 18:
             # switch offthe VMC
@@ -98,7 +136,7 @@ if __name__ == "__main__":
         #endregion
 
         #region check humidity in living room
-        humidity_living_room = float(home.rooms['living_room'].elements['sensor_humidity_living_room'].data['humidity'])
+        humidity_living_room = float(get_element_obj(home, 'living_room', 'sensor_humidity_living_room').data['humidity'])
         
         if humidity_living_room >= 0 and humidity_living_room < 25:
             # switch offthe VMC
@@ -109,4 +147,14 @@ if __name__ == "__main__":
             if vmc_living_room.data['state'] != "ON" :
                 home.mqtt_client.publish(vmc_living_room.topic , "ON")
         #endregion
+
+        #uniquement en mode manuel ??
+        unresolved_actions = check_and_format_actions_api(web_api)
+        if unresolved_actions != None:
+            for action in unresolved_actions:
+                element = get_element_obj(home, action['room'], action['element'])
+                #rajouter while acquitement 
+                home.mqtt_client.publish(element.topic, action["value"])
+                ## action réalisé on la passe à true en base
+                web_api.update_action(action["id"], action["value"], element.name, True)
 
