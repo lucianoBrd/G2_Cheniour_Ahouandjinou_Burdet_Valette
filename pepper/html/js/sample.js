dd00151666ce65b77aa3f37b06cfb88093b1c9c4
Lucien BURDET <lucien.burdet@cpe.fr>
2021-12-17 09:31:43 +0100

    fix bug check property exist

--- pepper/html/js/sample.js
+++ pepper/html/js/sample.js
@@ -14,7 +14,7 @@
 
                 $.each(homes, function (key, home) {
                     html += '<hr><div>';
-                    if (home.label !== 'undefined' && home.id !== 'undefined') {
+                    if (home.hasOwnProperty('label') && home.hasOwnProperty('id') && home.label !== 'undefined' && home.id !== 'undefined') {
                         html += '<button class="btn_choice_home" type="button" data-id="' + home.id + '">' + home.label + '</button>';
                     }
                     html += '</div><hr>';
@@ -31,36 +31,36 @@
                 var html = '';
                 const home = jQuery.parseJSON(data);
 
-                if (home.cameraip !== 'undefined' && home.cameraip !== null) {
+                if (home.hasOwnProperty('cameraip') && home.cameraip !== 'undefined' && home.cameraip !== null) {
                     src = 'http://' + home.cameraip + '/capture';
                 } else {
                     src = null;
                 }
 
-                if (home.label !== 'undefined') {
+                if (home.hasOwnProperty('label') && home.label !== 'undefined') {
                     html += '<h1>Home: ' + home.label + '</h1>';
                 }
 
-                if (home.rooms !== 'undefined') {
+                if (home.hasOwnProperty('rooms') && home.rooms !== 'undefined') {
                     html += '<div><h2>Liste des Rooms</h2><div>';
 
                     var rooms = '';
                     $.each(home.rooms, function (key, room) {
                         rooms += '<hr><div>';
-                        if (room.label !== 'undefined') {
+                        if (room.hasOwnProperty('label') && room.label !== 'undefined') {
                             rooms += '<p>Room: ' + room.label + '</p>';
                         }
 
                         var elements = '<h3>Liste des elements</h3>';
-                        if (room.elements !== 'undefined') {
+                        if (room.hasOwnProperty('elements') && room.elements !== 'undefined') {
                             elements += '<ul>';
                             $.each(room.elements, function (key, element) {
                                 elements += '<li>';
-                                if (element.label !== 'undefined') {
+                                if (element.hasOwnProperty('label') && element.label !== 'undefined') {
                                     elements += '<p>Element: ' + element.label + '</p>';
                                 }
                                 elements += '<p>Etat Actuel: ' + (element.state ? 'ON' : 'OFF') + '</p>';
-                                if (element.elementValue !== 'undefined' && element.elementValue !== null && element.elementValue.value !== 'undefined' && element.elementValue.datetime !== 'undefined') {
+                                if (element.hasOwnProperty('elementValue') && element.elementValue !== 'undefined' && element.elementValue !== null && element.elementValue.hasOwnProperty('value') && element.elementValue.hasOwnProperty('datetime') && element.elementValue.value !== 'undefined' && element.elementValue.datetime !== 'undefined') {
                                     var dateTime = element.elementValue.datetime;
 
                                     try {
@@ -76,7 +76,7 @@
 
                                     elements += '<p>Value: ' + element.elementValue.value + ' le ' + dateTime + '</p>';
                                 }
-                                if (element.type !== 'undefined' && element.type.label !== null && element.label !== 'undefined') {
+                                if (element.hasOwnProperty('type') && element.hasOwnProperty('label') && element.type !== 'undefined' && element.type.label !== null && element.label !== 'undefined') {
                                     var type = element.type.label;
                                     var action = null;
                                     var state = null;
@@ -84,7 +84,7 @@
                                     elements += '<p>Type: ' + type + '</p>';
 
                                     elements += '<div>Action: ';
-                                    if (element.action !== 'undefined' && element.action !== null && element.action.value !== 'undefined' && element.action.state !== 'undefined') {
+                                    if (element.hasOwnProperty('action') && element.action !== 'undefined' && element.action !== null && element.action.hasOwnProperty('value') && element.action.hasOwnProperty('state') && element.action.value !== 'undefined' && element.action.state !== 'undefined') {
                                         action = element.action.value;
                                         state = element.action.state;
                                     }
